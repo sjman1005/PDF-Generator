27ebb2e72e29ac9af13553187044936a4a2036b0
Sam M Samuel <133142882+sjman1005@users.noreply.github.com>
2024-07-29 21:42:45 +0530

    Added Actions

--- APP/views.py
+++ APP/views.py
@@ -8,32 +8,33 @@ def generate(request):
     return render(request, 'APP/generate.html')
 
 # views.py
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
 from .forms import PDFForm
 from xhtml2pdf import pisa
 from io import BytesIO
 
-def generate_pdf(request):
-    if request.method == 'POST':
+class GeneratePdf(View):
+    def get(self, request, *args, **kwargs):
+        form = PDFForm()
+        return render(request, 'APP/generate.html', {'form': form})
+
+    def post(self, request, *args, **kwargs):
         form = PDFForm(request.POST)
         if form.is_valid():
             title = form.cleaned_data['title']
             content = form.cleaned_data['content']
-            
+
             # Create a PDF response
-            response = HttpResponse(content_type='APP/pdf')
+            response = HttpResponse(content_type='application/pdf')
             response['Content-Disposition'] = f'attachment; filename="{title}.pdf"'
 
             # Render content to PDF
             template = f"<h1>{title}</h1><p>{content}</p>"
             pisa_status = pisa.CreatePDF(BytesIO(template.encode('utf-8')), dest=response)
-            
-            if pisa_status.err:
-                return HttpResponse('We had some errors with your PDF generation')
-            return response
-    else:
-        form = PDFForm()
-    
-    return render(request, 'index.html', {'form': form})
 
+            if pisa_status.err:
+                return HttpResponse('We had some errors with your PDF generation', content_type='text/plain')
+            return response
+        else:
+            return render(request, 'APP/generate.html', {'form': form})

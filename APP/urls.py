a430ea07544919a95803f949035d9f76065635cf
Sam M Samuel <133142882+sjman1005@users.noreply.github.com>
2024-07-29 21:43:57 +0530

    Changed the line 11 code as per views.py

--- APP/urls.py
+++ APP/urls.py
@@ -1,11 +1,12 @@
-from django.contrib import admin 
-from django.urls import path 
-  
-# importing views from views..py 
+from django.contrib import admin
+from django.urls import path
+
+# Importing views from views.py
 from . import views
-  
-urlpatterns = [ 
-    path('',  views.index, name="index"),
+from .views import GeneratePdf
+
+urlpatterns = [
+    path('', views.index, name="index"),
     path('generate', views.generate, name="generate"),
-    path('generate', views.generate_pdf, name="generate_pdf"),
+    path('GeneratePdf/', GeneratePdf.as_view(), name="GeneratePdf"),
 ]

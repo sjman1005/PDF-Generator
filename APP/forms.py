825da4ccb07a9436e9147af16f5b6c4894895924
Sam M Samuel <133142882+sjman1005@users.noreply.github.com>
2024-07-30 12:08:15 +0530

    Updated forms.py

--- APP/forms.py
+++ APP/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class PDFForm(forms.Form):
-    title = forms.CharField(label='Title', max_length=100)
-    content = forms.CharField(label='Content', widget=forms.Textarea)
+    title = forms.CharField(max_length=100)
+    content = forms.CharField(widget=forms.Textarea)
